a12aae1dd57136d7dc2ca3903122fd0de48382d5
umbe1987 <umbertofilippo@tiscali.it>
2022-09-11 20:40:47 +0200

    code refactor, and use of SAT buffer

--- Hello World.asm
+++ Hello World.asm
@@ -5,14 +5,19 @@ VDPControl equ $bf
 VdpData equ $be
 VRAMWrite equ $4000
 CRAMWrite equ $c000
-SATY equ $3f00          ; start of Sprite Attribute Table Y coords (/$7f00)
-SATX equ $3f80          ; start of Sprite Attribute Table X coords (/$7f80)
-SATN equ $3f81          ; start of Sprite Attribute Table Sprite Number pairs (/$7f81)
+SAT equ $3f00           ; starting location of SAT in VRAM
+ply equ $c100           ; player Y (end of SAT buffer, which is $c000-c0ff)
+plx equ $c101           ; player X
 
-ply equ $60             ; player initial Y position
-plx equ $80             ; player initial X position
+; Map of the sprite attribute table (sat) buffer.
+; Contains sprites' vertical position (vpos), horizontal posi-
+; tion (hpos) and character codes (cc).
 
-endspr equ $3F0C          ; first unused sprite.
+plrvp equ $c000         ; first player vpos.
+plrhp equ $c080         ; first player hpos.
+plrcc equ $c081         ; first player cc.
+
+endspr equ $c00c       ; first unused sprite.
 
     org $0000
 ;==============================================================
@@ -109,29 +114,29 @@ main:
     call CopyToVDP
 
     ;==============================================================
-    ; Write sprite to sprite attribute table (SAT)
+    ; Initialize player position
     ;==============================================================
-    ld hl,SATY | VRAMWrite                  ; Start location of Y coordinates table in VRAM $3f00-$3f3f (64 bytes for 64 sprites)
-    call SetVDPAddress
 
-    ld a,ply
-    call PlayerSprY
+    ld a,120
+    ld (ply),a        ; set player Y.
 
-    ld hl,SATX | VRAMWrite                  ; Start location of X coordinates table in VRAM $3f80 (64 bytes for 64 sprites)
-    call SetVDPAddress
+    ld a,120
+    ld (plx),a        ; set player X.
 
-    ld a,plx
-    call PlayerSprX
+    ;==============================================================
+    ; Put main character in the SAT BUFFER
+    ;==============================================================
 
-    ld hl,SATN | VRAMWrite                  ; Start location of Sprite Number table in VRAM $3f81 (64 bytes for 64 sprites)
-    call SetVDPAddress
-    
-    ld de,PlayerN
-    ld bc,PlayerNEnd-PlayerN                ; Counter for number of bytes to write
-    call PlayerSprN
+    ld hl,plrcc                 ; point to player cc in buffer.
+    ld de,PlayerN               ; point to player graphics.
+    ld bc,PlayerNEnd-PlayerN    ; Counter for number of bytes to write
+    call PlayerSprN             ; set the char codes for player in buffer.
 
     ld hl,endspr               ; point to end of active sprites.
     ld (hl),$d0                ; insert sprite terminator here.
+
+    call UpdateSATBuff         ; update SAT buffer.
+    call LoadSAT               ; load sat from buffer.
 
     ; Turn screen on
     ld a,%01000000
@@ -178,15 +183,43 @@ CopyToVDP:
     jr nz,CopyToVDP
     ret
 
+LoadSAT:
+; Load SPRITE ATTRIBUTE TABLE
+; Load data into sprite attribute table (SAT) from the buffer.
+    ld hl,SAT | VRAMWrite ; point to start of SAT in vram.
+    call SetVDPAddress    ; prepare vram to recieve data.
+    ld b,255              ; amount of bytes to output.
+    ld c,VdpData          ; destination is vdp data port.
+    ld hl,plrvp
+
+    otir                  ; output buffer to vdp.
+    
+    ret
+
+UpdateSATBuff:
+; Generate vpos, hpos and cc data for the sprites.
+; Generate sat buffer data from player's x,y coordinates.
+; INFO: we don't have to update characters' CC, only their XY positions
+    ld a,(ply)          ; load player's current y-coordinate.
+    ld hl,plrvp         ; buffer address of player vpos.
+    call PlayerSprY     ; refresh buffer according to y.
+
+    ld a,(plx)          ; load player's current x-coordinate.
+    ld hl,plrhp         ; buffer address of player hpos.
+    call PlayerSprX     ; refresh buffer according to x.
+
+    ret
+
 PlayerSprY:
-; player Y SPRITES' VERTICAL IN SAT
+; player Y SPRITES' VERTICAL IN SAT Buffer
 ; Parameters: hl = SATY address, a = ply
 ; Affects: a, hl, b
     rept 4                     ; repeat 4 times as player is 4 tiles tall
     inline                     ; use local name space (VASM does not have local name space like WLA-DX)
     ld b,3                     ; counter 3 as player is tile wide
     .PlayerSprYLoop:
-        out (VDPData),a
+        ld (hl),a              ; write y pos to buffer
+        inc hl                 ; next Y coord
         djnz .PlayerSprYLoop   ; decrease B and jump back if NonZero
     add a,8                    ; add 8 (since next row is 8 pixels below)
     einline                    ; end of local name space (http://eab.abime.net/showthread.php?t=88827)
@@ -197,7 +230,7 @@ PlayerSprY:
 ; --------------------------------------------------------------
 
 PlayerSprX:
-; player X SPRITES' HORIZONTAL IN SAT
+; player X SPRITES' HORIZONTAL IN SAT Buffer
 ; Parameters: hl = SATX address, a = plx
 ; Affects: a, hl, b
     ld c,a                     ; save initial X coordinate in C
@@ -206,10 +239,9 @@ PlayerSprX:
     ld a,c                     ; start from the initial X coordinate saved in C
     ld b,3                     ; counter 3 as player is tile wide
     .PlayerSprXLoop:
-        out (VDPData),a
+        ld (hl),a              ; write x pos to buffer
         inc hl                 ; first SATX increase points to sprite number byte
         inc hl                 ; second SATX increase point to next X coord byte
-        call SetVDPAddress
         add a,8                ; add 8 (since next column is 8 pixels apart)
         djnz .PlayerSprXLoop   ; decrease B and jump back if NonZero
     einline                    ; end of local name space (http://eab.abime.net/showthread.php?t=88827)
@@ -218,15 +250,14 @@ PlayerSprX:
     ret
 
 PlayerSprN:
-; player N SPRITES' IN SAT
-; Parameters: de = SATN address, bc = data length
+; player N SPRITES' IN SAT Buffer
+; Parameters: de = SAT buffer index, bc = data length, hl = pointer to 12 byte char codes block
 ; Affects: a, hl, bc, de
     ld a,(de)                  ; Get data byte
-    out (VDPData),a
+    ld (hl),a
     inc de                     ; point to next sprite index
     inc hl                     ; first increase points to X coord byte
     inc hl                     ; second increase point to next sprite number byte
-    call SetVDPAddress
     dec bc
     ld a,c
     or b
